923d55bf5ef9a8d6eb8d60a4949594f9c74b861d
JordanBWilson <jordanblairwilson@gmail.com>
2021-08-14 15:58:10 -0400

    made everything more modular. working on the breakout game demo

---
+++ js/arurora_global_vars.js
@@ -0,0 +1,18 @@
+
+let Game = { // the user will want to play with this object
+  frameRate: 1000 / 60, // 60 frames a second
+  methodsToRun: [], // all the methods to make the game run
+  canvas: undefined, // need the game stage
+  stageWidthPrct: .90, // how much of the screen width will it take up
+  stageHeightPrct: .80, // how much of the screen height will it take up
+};
+
+let Main = { // global variables to keep the game running nicely
+  interval: undefined, // the main loop running
+  intervalAnimateId: undefined, // the main loop's animation id
+  stage: undefined, // the 2D game stage
+  resizeWindow: undefined, // when the browser window gets resized
+  resizeWindowTime: 250, // how long to wait for the browser to resize
+  entitySize: 0, // this is a base standard for how big an object will be drawn to the screen
+  entityWidth: 0, // this is the base standard for the width of an entity
+};
--- js/arurora_main.js
+++ js/arurora_main.js
@@ -1,29 +1,19 @@
 
-let Game = { // the user will want to play with this object
-  frameRate: 1000 / 60, // 60 frames a second
-  methodsToRun: [], // all the methods to make the game run
-  canvas: document.getElementById('Stage'), // need the game stage
-  stageWidthPrct: .90, // how much of the screen width will it take up
-  stageHeightPrct: .80, // how much of the screen height will it take up
-};
-
-let Global = { // global variables to keep the game running nicely
-  interval: undefined,
-  intervalAnimateId: undefined,
-  stage: undefined,
-  resizeTime: undefined
-};
-
 (function() {
   if (!Game.canvas) {
-    console.log('No game stage detected. Add a canvas tag.');
+    console.log('No game stage detected.');
+    console.log('To add a game stage, create a canvas tag on your html file.');
+    console.log('Then assign it to the game like this:');
+    console.log('_________________________________________');
+    console.log('Game.canvas = document.getElementById("Stage");');
+    console.log('_________________________________________');
   } else {
     // get this party train moving
-    // let testMethod = {method: function() {console.log('test');}}
+    // let method = {method: function() {console.log('test');}}
     // Game.methodsToRun.push(testMethod);
     window.addEventListener('resize', resizeStage, false);
     resizeStage();
-    Global.stage = Game.canvas.getContext('2d');
+    Main.stage = Game.canvas.getContext('2d');
 
     console.log(Game);
     mainLoop();
@@ -31,7 +21,7 @@ let Global = { // global variables to keep the game running nicely
 })();
 
 function mainLoop() {
-  Global.interval = setInterval(function() {
+  Main.interval = setInterval(function() {
     if (Game.methodsToRun.length > 0) {
       // run the game
       for (let i = 0; i < Game.methodsToRun.length; i++) {
@@ -40,18 +30,27 @@ function mainLoop() {
       }
     } else {
       // stop the game
-      console.log('The game has stopped. No more methods to listen to');
-      clearInterval(Global.interval);
-      cancelAnimationFrame(Global.intervalAnimateId);
+      console.log('The game has stopped. No more methods to listen to.');
+      console.log('Assign new methods to the game like this:');
+      console.log('_________________________________________');
+      console.log('let method = {method: function() {console.log("test");}};');
+      console.log('Game.methodsToRun.push(method);');
+      console.log('_________________________________________');
+      console.log('Now you will see the the word test being ran indefinetly in the console.');
+      clearInterval(Main.interval);
+      cancelAnimationFrame(Main.intervalAnimateId);
     }
   }, Game.frameRate);
 }
 
 function resizeStage() {
   // don't want to grab the new width and height too many times..
-  clearTimeout(Global.resizeTime);
-  Global.resizeTime = setTimeout(function() {
+  clearTimeout(Main.resizeWindow);
+  Main.resizeWindow = setTimeout(function() {
+    // resize the game stage and set new base values
     Game.canvas.width = window.innerWidth * Game.stageWidthPrct;
     Game.canvas.height = window.innerHeight * Game.stageHeightPrct;
-  }, 250);
+    Main.entitySize = (Game.canvas.height * 0.01);
+    Main.entityWidth = (Game.canvas.width * 0.01);
+  }, Main.resizeWindowTime);
 }
