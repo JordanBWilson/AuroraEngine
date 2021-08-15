b68c6ff38500ab48729d57007f0090ea6e37910f
JordanBWilson <jordanblairwilson@gmail.com>
2021-08-15 16:05:04 -0400

    made the main menu. clean up

--- js/arurora_engine/arurora_2d_core.js
+++ js/arurora_engine/arurora_2d_core.js
@@ -1,26 +1,28 @@
 
 // draws text to the stage and only redraws it if the stage has been resized
-function drawText(font, msg, posX, posY, color, methodId) { // ex: '48px serif', 'Hello', 10, 50, black
+// ex: '48px serif', 'Hello', 10, 50, 'black', 'start'
+function drawText(font, msg, posX, posY, color, align, methodId) {
   if (!Main.methodParams[methodId] || Main.isResizing ||
     Main.methodParams[methodId].font !== font ||
     Main.methodParams[methodId].msg !== msg ||
     Main.methodParams[methodId].posX !== posX ||
     Main.methodParams[methodId].posY !== posY ||
-    Main.methodParams[methodId].color !== color
+    Main.methodParams[methodId].color !== color ||
+    Main.methodParams[methodId].align !== align
   ) {
-    console.log(Main.isResizing);
     Main.stage.fillStyle = color;
     Main.stage.font = font;
+    Main.stage.textAlign = align;
     Main.stage.fillText(msg, posX, posY);
   }
-
   if (!Main.methodParams[methodId]) {
     let params = {
       font: font,
       msg: msg,
       posX: posX,
       posY: posY,
-      color: color
+      color: color,
+      align: align
     }
     Main.methodParams.push(params);
   } else {
@@ -29,5 +31,53 @@ function drawText(font, msg, posX, posY, color, methodId) { // ex: '48px serif',
     Main.methodParams[methodId].posX = posX;
     Main.methodParams[methodId].posY = posY;
     Main.methodParams[methodId].color = color;
+    Main.methodParams[methodId].align = align;
+  }
+}
+// this will draw a rectangle to the screen
+// ex: 9, 51, 100, 100, 1, 'green', false
+function drawRect(posX, posY, width, height, lineWidth, color, isFilled, methodId) {
+  if (!Main.methodParams[methodId] || Main.isResizing ||
+    Main.methodParams[methodId].posX !== posX ||
+    Main.methodParams[methodId].posY !== posY ||
+    Main.methodParams[methodId].width !== width ||
+    Main.methodParams[methodId].height !== height ||
+    Main.methodParams[methodId].isFilled !== isFilled
+  ) {
+    Main.stage.beginPath();
+    if (!lineWidth) {
+      Main.stage.lineWidth = '1';
+    } else {
+      Main.stage.lineWidth = lineWidth;
+    }
+    Main.stage.rect(posX, posY, width, height);
+    if (isFilled) {
+      Main.stage.fillStyle = color;
+      Main.stage.fill();
+    } else {
+      Main.stage.strokeStyle = color;
+      Main.stage.stroke();
+    }
+  }
+
+  if (!Main.methodParams[methodId]) {
+    let params = {
+      posX: posX,
+      posY: posY,
+      width: width,
+      height: height,
+      lineWidth: lineWidth,
+      color: color,
+      isFilled: isFilled,
+    }
+    Main.methodParams.push(params);
+  } else {
+    Main.methodParams[methodId].posX = posX;
+    Main.methodParams[methodId].posY = posY;
+    Main.methodParams[methodId].width = width;
+    Main.methodParams[methodId].height = height;
+    Main.methodParams[methodId].lineWidth = lineWidth;
+    Main.methodParams[methodId].color = color;
+    Main.methodParams[methodId].isFilled = isFilled;
   }
 }
--- js/breakout.js
+++ js/breakout.js
@@ -1,9 +1,16 @@
 
 (function() {
-  Game.canvas = document.getElementById('Stage');
 
-  let method = {method: function(id) {drawText('48px serif', 'Hello world', 10, 50, 'black', id);}}
-  let method1 = {method: function(id) {drawText('48px serif', 'Hello world', 10, 100, 'green', id);}}
-  Game.methodsToRun.push(method);
-  Game.methodsToRun.push(method1);
+  Game.canvas = document.getElementById('Stage');
+  drawMainMenu();
 })();
+
+function drawMainMenu() {
+
+  let backgroundColor = {method: function(id) {drawRect(0, 0, Game.canvas.width, Game.canvas.height, 1, 'black', true, id);}}
+  Game.methodsToRun.push(backgroundColor);
+  let majorTitle = {method: function(id) {drawText('250% serif', 'Breakout', (Game.canvas.width * .5), (Game.canvas.height * 0.1), 'green', 'center', id);}}
+  let minorTitle = {method: function(id) {drawText('16px serif', 'An Arurora Engine Demo', (Game.canvas.width * .5), (Game.canvas.height * 0.14), 'green', 'center', id);}}
+  Game.methodsToRun.push(majorTitle);
+  Game.methodsToRun.push(minorTitle);
+}
