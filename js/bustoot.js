39d8b34789d56089f30b064f5c3a11c0711913be
JordanBWilson <jordanblairwilson@gmail.com>
2021-11-22 08:31:15 -0500

    working on fixing menu transitions

--- js/bustoot.js
+++ js/bustoot.js
@@ -511,6 +511,7 @@ function drawLoseMenu() {
         }
      };
   Game.methodsToRun.push(playBtn);
+  console.log(Game.methodObjects);
 }
 
 function drawWinMenu() {
@@ -546,7 +547,7 @@ function drawWinMenu() {
 
 function drawMainMenu() { // draw the main menu
   Game.clearStage();
-  Game.setSettingsLow();
+  Game.setSettingsHigh();
   const backgroundColor = { method: function(id) {drawRect(0, 0, Game.canvas.width, Game.canvas.height, 1, 'black', true, 'menu-background', false, false, false, {}, id);} };
   Game.methodsToRun.push(backgroundColor);
   const majorTitle = { method: function(id) {drawText('3rem serif', 'Bustoot', (Game.canvas.width * 0.5), (Game.canvas.height * 0.1), 'green', 'center', false, {}, id);} };
