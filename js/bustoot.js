892f553aa92a4012c43663dfb8313e48527209a0
JordanBWilson <jordanblairwilson@gmail.com>
2021-11-28 13:29:08 -0500

    doing more tests...

--- js/bustoot.js
+++ js/bustoot.js
@@ -133,6 +133,7 @@ function playGame() { // draw the game
   const playGameBall = { method: function(id) { moveGameBall(); }};
   Game.methodsToRun.push(playGameBall);
   
+  
   // const backgroundColorTop = { method: function(id) {if (backgroundTop.methodId === undefined){backgroundTop.methodId = id;} drawRect(backgroundTop.posX, backgroundTop.posY, backgroundTop.width, backgroundTop.height, backgroundTop.lineWidth, backgroundTop.color, backgroundTop.isFilled, backgroundTop.id, backgroundTop.isSolid, backgroundTop.isAnim, backgroundTop.isBackground, backgroundTop.props, backgroundTop.methodId);} };
   // const backgroundColorBot = { method: function(id) {if (backgroundBot.methodId === undefined){backgroundBot.methodId = id;} drawRect(backgroundBot.posX, backgroundBot.posY, backgroundBot.width, backgroundBot.height, backgroundBot.lineWidth, backgroundBot.color, backgroundBot.isFilled, backgroundBot.id, backgroundBot.isSolid, backgroundBot.isAnim, backgroundBot.isBackground, backgroundBot.props, backgroundBot.methodId);} };
   const backgroundColor = { method: function(id) {drawRect(0, 0, Game.canvas.width, Game.canvas.height, 1, 'black', true, 'background', false, false, true, {}, id);} };
@@ -257,10 +258,10 @@ function moveGameBall() {
         ball.props.direction = 'bot';
       }
       if (gameLives === 0) {
-        setTimeout(function() {
+        // setTimeout(function() {
           drawLoseMenu();
           gameLives = -1;
-        },100);
+        // },100);
         
       }
     }
