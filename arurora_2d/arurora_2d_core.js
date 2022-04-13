0f8312d7064ccc2dc581cd4afff692a57e9c1b0b
JordanBWilson <jordanblairwilson@gmail.com>
2022-04-12 20:22:19 -0400

    added license notices to all the source files

--- arurora_2d/arurora_2d_core.js
+++ arurora_2d/arurora_2d_core.js
@@ -1,3 +1,18 @@
+// Copyright (C) 2022  Jordan Wilson
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 // this will draw objects to the screen and only redraws them if the stage has been resized
 // or an animation is happening on stage
 // draws text to the stage
--- mason_single_player/js/mason.js
+++ mason_single_player/js/mason.js
@@ -1,3 +1,17 @@
+// Copyright (C) 2022  Jordan Wilson
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 const masonWorkerImg = new Image();
 const rockImg = new Image();
 const grassImg = new Image();
