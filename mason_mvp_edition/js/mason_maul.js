9850baa6d90009886e008836f3cd694d7b0a4fbf
JordanBWilson <jordanblairwilson@gmail.com>
2024-01-09 21:10:31 -0500

    testing the different images

--- mason_mvp_edition/js/mason_maul.js
+++ mason_mvp_edition/js/mason_maul.js
@@ -1864,7 +1864,8 @@ const maulPage = {
 		
 		function sendRobot(robot) {
 			// const chassis = drawRobotSelectPreviewParts('chassis', robot.robotParts, true);
-			alert(robotChassis[0].imgs.length, Aurora.gifImageList.length);
+			alert(robotChassis[0].imgs.length);
+			alert(Aurora.gifImageList.length);
 			//Aurora.methodSetup = {
 				//method: function(id) {
 					//drawImage({
